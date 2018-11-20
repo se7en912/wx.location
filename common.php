4c3575857d4694de2f1fedc9613d0e51493914f3
Czh <523769360@qq.com>
2018-11-20 10:46:33 +0800

    Update common.php

--- common.php
+++ common.php
@@ -4,7 +4,9 @@ public function jssdk(){
     global $signPackage;
 
 	require_once ROOT_PATH."/jssdk.php";
+	//开发者ID(AppID)
 	$APPID = '**';
+	//开发者秘钥(APPSECRET)
 	$APPSECRET = '**';
 	
 	$jssdk = new JSSDK($APPID, $APPSECRET);
